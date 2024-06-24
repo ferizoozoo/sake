25a08823b45e55dfacdc5b12a31beef7900ad7f7
ferizoozoo <farhadzohoorz@gmail.com>
2024-06-24 20:29:31 +0330

    feat: add router and basic registering of routes with their handlers

--- main.go
+++ main.go
@@ -9,9 +9,23 @@ import (
 func main() {
 	fmt.Println("Hello to Sake!")
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Request to / received")
-		io.WriteString(w, "Hello to Sake!")
-	})
+
+	handlerFuncs := []RouteHandlers{
+		{
+			"/",
+			func(w http.ResponseWriter, r *http.Request) {
+				fmt.Println("Request to / received")
+				io.WriteString(w, "Hello to Sake!")
+			},
+		},
+		{
+			"/welcome",
+			func(w http.ResponseWriter, r *http.Request) {
+				fmt.Println("Request to /welcome received")
+				io.WriteString(w, "Welcome to Sake!")
+			},
+		},
+	}
+	RegisterRoutes(mux, handlerFuncs)
 	_ = http.ListenAndServe(":8000", mux)
 }
---
+++ router.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+type RouteHandlers struct {
+	route   string
+	handler http.HandlerFunc
+}
+
+func RegisterRoutes(mux *http.ServeMux, routeHandlers []RouteHandlers) {
+	for _, rh := range routeHandlers {
+		mux.HandleFunc(rh.route, rh.handler)
+	}
+}
