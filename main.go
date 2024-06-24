4a5439058c2c0ea124ed1eaaeee657353cbe66f2
ferizoozoo <farhadzohoorz@gmail.com>
2024-06-24 12:07:55 +0330

    feat: simple http server with root route

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello to Sake!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Request to / received")
+		io.WriteString(w, "Hello to Sake!")
+	})
+	_ = http.ListenAndServe(":8000", mux)
 }
